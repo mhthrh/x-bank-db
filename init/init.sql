f27ddd50e46be1cc2a88dba76544e250c286cfb4
Mohsen <mhthrh@gmail.com>
2025-02-03 10:46:09 -0800

    add test db

--- init/init.sql
+++ init/init.sql
@@ -1,3 +1,6 @@
 create database x_bank;
+create database db_test;
+CREATE USER daste_khar WITH PASSWORD 'daste_khar_test';
+GRANT ALL PRIVILEGES ON DATABASE db_test TO daste_khar;
 CREATE ROLE admin_role WITH LOGIN PASSWORD '123456';
 ALTER ROLE admin_role WITH SUPERUSER;
